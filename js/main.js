546da41f8b46dbfc2ac66c00ecbedfbaf463502b
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 07:38:39 -0800

    das

--- js/main.js
+++ js/main.js
@@ -27,8 +27,11 @@ function makeCorsRequest() {1
   var bad_words = ["all", "another", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million"];
   var i;
   for (i = 0; i < word.length; ++i) {
-      // do something with `substr[i]`
+    if(isInArray(word[i], bad_words) > -1) {
+      word.splice(i);
+    }
   }
+  console.log(word);
 
 }
 $(".btn").click(function(){
