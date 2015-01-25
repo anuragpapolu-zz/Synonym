c92efc1123921136565b8ab1d054075ea1bc73a1
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-24 18:06:14 -0800

    dsa

--- php/get.php
+++ php/get.php
@@ -4,7 +4,7 @@
 function grab_xml_definition ($word, $ref, $key)
 	{	$uri = "http://www.dictionaryapi.com/api/v1/references/" . urlencode($ref) . "/xml/" . 
 					urlencode($word) . "?key=" . urlencode($key);
-		return file_get_contents($uri);
+		return $uri;
 	};
 
 $xdef = grab_xml_definition($_GET['word'], "thesaurus", "f10db1b0-2697-4971-9302-863a3d90f4c1");
