7dea5a221017a0f92f67162b53eaa0d1379a72af
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-24 22:26:46 -0800

    asd

--- js/main.js
+++ js/main.js
@@ -18,38 +18,29 @@ function getTitle(text) {
   var list = eval('(' + text+ ')');
   return list;
 }
+function isInArray(value, array) {
+  return array.indexOf(value);
+}
 // Make the actual CORS request.
 function makeCorsRequest(word) {
 // All HTML5 Rocks properties support CORS.
-  $.each(word, function(index, value){
-    var url = 'http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/'+value+'/json';
-    $.ajax({
+  var words = $.unique(word.match(/\w+/mg));
+  var random = [];
 
-      type: 'GET',
-      url: url,
-      contentType: 'json',
-      xhrFields: {
-        // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-        // This can be used to set the 'withCredentials' property.
-        // Set the value to 'true' if you'd like to pass cookies to the server.
-        // If this is enabled, your server must respond with the header
-        // 'Access-Control-Allow-Credentials: true'.
-        withCredentials: false
-      },
-      success: function(data) {
-          var text = data;
-          var title = getTitle(text);
-          console.log(title);
-      },
-
-    });
+  for(var i=0; i<5; i++) {
+      var rn = Math.floor(Math.random() * words.length);
+      random.push( words[rn]);
+      words.splice(rn, 1);
+  }
+  $(".wrapper").text().split(" ").each(function(index, value){
+    if(isInArray(value, random) > -1) {
+      random.splice([isInArray(value, random)]);
+      value = "<span>"+value+"</span>";
+    }
   });
+
 
 }
 $(".btn").click(function(){
   makeCorsRequest($(".wrapper").text().split(" "));
 });
-
-
-
-
