14394721691dc8581ddff00c05bd50b1c2c94998
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-24 17:24:21 -0800

    Das

--- js/main.js
+++ js/main.js
@@ -17,13 +17,14 @@ function createCORSRequest(method, url) {
 
 // Helper method to parse the title tag from the response.
 function getTitle(text) {
-  var list = eval('(' + text+ ')');
+  var list = text
   return list;
 }
 
 // Make the actual CORS request.
 function makeCorsRequest(word) {
   var results = [];
+  word = eval('(' + word + ')');
   $.each(word, function (index, value) {
     var url = 'http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/'+value+'/json';
 
@@ -38,7 +39,7 @@ function makeCorsRequest(word) {
       var text = xhr.responseText;
       var title = getTitle(text);
       results.push(title);
-      console.log(results)
+      console.log(results);
     };
 
     xhr.onerror = function() {
