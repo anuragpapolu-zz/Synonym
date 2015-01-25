cc04f1b63007db1d5ce0e397f0ece13290033652
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 06:52:56 -0800

    DSa

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function isInArray(value, array) {
 function makeCorsRequest(word) {
 // All HTML5 Rocks properties support CORS.
   var bad_words = ["all", "another", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million"];
-  console.log(word);
+  console.log($(".wrapper").text().split(" "));
   $.each(word, function(index, value){
     if(isInArray(value, bad_words) > -1) {
       word.splice(index);
