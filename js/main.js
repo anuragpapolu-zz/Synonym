18afa3144a46bd43d2eb17b3bacb55815e8d3751
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 08:54:18 -0800

    DSa

--- js/main.js
+++ js/main.js
@@ -30,21 +30,16 @@ function makeCorsRequest() {
   var text = $(".wrapper").text();
   var word = $.trim(text.replaceAll(',', ' ').replaceAll('"', ' ').replaceAll('!', ' ').replace('.', ' ').replace('?', ' ')).split(" ");
   var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million"];
-  console.log(word);
   var i;
-  for (i = 0; i < word.length; ++i) {
-    if(isInArray(word[i], bad_words) > -1) {
-      word.splice(i,1);
-    } else if (word[i] == "") {
-      word.splice(i,1);
+  while(word.length>0) {
+    for (i = 0; i < word.length; ++i) {
+      if(isInArray(word[i], bad_words) > -1) {
+        word.splice(i,1);
+      } else if (word[i] == "") {
+        word.splice(i,1);
+      }
     }
-  }
-  for (i = 0; i < word.length; ++i) {
-    if(isInArray(word[i], bad_words) > -1) {
-      word.splice(i,1);
-    } else if (word[i] == "") {
-      word.splice(i,1);
-    }
+
   }
 
   console.log(word);
