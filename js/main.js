0983a36e52d9fc03a43965cba1039c8206761260
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 13:49:22 -0800

    das

--- js/main.js
+++ js/main.js
@@ -90,5 +90,6 @@ $(document).on('click', '#results a', function(){
     var value = $(this).attr("href").replace("#","");
     var replacement = $(this).text();
     $('.wrapper a[href="#'+value+'"]').text(replacement);
+    $('.wrapper a[href="#'+value+'"]').attr('href', '#'+replacement);
     $("#results").html('<div class="list-group"></div>');
  });
