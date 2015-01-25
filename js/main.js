3868f6fb4522fe39a780102bd37be68f7df8ebee
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 08:07:13 -0800

    as

--- js/main.js
+++ js/main.js
@@ -18,13 +18,17 @@ function getTitle(text) {
   var list = eval('(' + text+ ')');
   return list;
 }
+String.prototype.replaceAll = function(str1, str2, ignore) 
+{
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+} 
 function isInArray(value, array) {
   return array.indexOf(value);
 }
 // Make the actual CORS request.
 function makeCorsRequest() {
   var text = $(".wrapper").text();
-  var word = $.trim(text.replace(/,/g, ' ').replace(/!/g, ' ').replace(/./g, ' ').replace(/?/g, ' ')).split(" ");
+  var word = $.trim(text.replace(',', ' ').replaceAll('!', ' ').replace('.', ' ').replace('?', ' ')).split(" ");
   var bad_words = ["all", "another", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million"];
   var i;
 
