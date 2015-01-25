a4008a86f418f7b84ace0b27ca1ed0f3f4e34a7c
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-24 21:03:35 -0800

    DAS

--- js/main.js
+++ js/main.js
@@ -1,47 +1,43 @@
 function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
   if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
+// XHR for Chrome/Firefox/Opera/Safari.
+xhr.open(method, url, true);
+} else if (typeof XDomainRequest != "undefined") {
+// XDomainRequest for IE.
+xhr = new XDomainRequest();
+xhr.open(method, url);
+} else {
+// CORS not supported.
+xhr = null;
 }
-
+return xhr;
+}
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+  var list = eval('(' + text+ ')');
+  return list;
+}
 // Make the actual CORS request.
 function makeCorsRequest(word) {
-  // All HTML5 Rocks properties support CORS.
-  var results = [];
-  $.each(word, function (index, value) {
-    var url = "http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/"+value+"/json/";
-    var xhr = createCORSRequest('GET', url);
-    if (!xhr) {
-      alert('CORS not supported');
-      return;
-    }
-    xhr.onload = function() {
-      var text = xhr.responseText;
-      results.push(text);
-    };
-
-    xhr.onerror = function() {
-      alert('Woops, there was an error making the request.');
-    };
-
-    xhr.send();    
-  });
-
-  console.log(results);
-  // Response handlers.
-
+// All HTML5 Rocks properties support CORS.
+var url = 'http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/'+word+'/json';
+var xhr = createCORSRequest('GET', url);
+if (!xhr) {
+  console.log('CORS not supported');
+  return;
 }
-
+// Response handlers.
+xhr.onload = function() {
+  var text = xhr.responseText;
+  var title = getTitle(text);
+  console.log(title);
+};
+xhr.onerror = function() {
+  console.log('Woops, there was an error making the request.');
+};
+xhr.send();
+}
 $(".wrapper").bind('input propertychange', function(){
-  makeCorsRequest($(this).text().split(" "));
+  makeCorsRequest($(this).text());
 });
