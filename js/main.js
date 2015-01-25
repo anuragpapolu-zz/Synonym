2ff1947218aac864bda766e2159c01b6866b24a0
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 06:54:47 -0800

    dsa

--- js/main.js
+++ js/main.js
@@ -22,10 +22,9 @@ function isInArray(value, array) {
   return array.indexOf(value);
 }
 // Make the actual CORS request.
-function makeCorsRequest(word) {
-// All HTML5 Rocks properties support CORS.
+function makeCorsRequest() {
+  var word = $(".wrapper".text().split(" ");
   var bad_words = ["all", "another", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million"];
-  console.log($(".wrapper").text().split(" "));
   $.each(word, function(index, value){
     if(isInArray(value, bad_words) > -1) {
       word.splice(index);
@@ -36,5 +35,5 @@ function makeCorsRequest(word) {
 
 }
 $(".btn").click(function(){
-  makeCorsRequest($(".wrapper").text().split(" "));
+  makeCorsRequest();
 });
