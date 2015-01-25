3e9de3376b3fe58d10124f2637c467388b7e4a56
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 07:57:05 -0800

    ADS

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@ function isInArray(value, array) {
   return array.indexOf(value);
 }
 // Make the actual CORS request.
-function makeCorsRequest() {1
-  var word = $.trim($(".wrapper").text()).replace(",", " ").replace("!", " ").replace(".", " ").replace("?", " ").split(" ");
+function makeCorsRequest() {
+  var text = $(".wrapper").text();
+  var word = $.trim(text.replace(",", " ").replace("!", " ").replace(".", " ").replace("?", " ")).split(" ");
   var bad_words = ["all", "another", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million"];
   var i;
 
