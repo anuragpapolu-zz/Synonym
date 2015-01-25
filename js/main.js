d54606817c84399a78ad4cdd13c730eb12c8c96d
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-24 22:01:59 -0800

    ads

--- js/main.js
+++ js/main.js
@@ -23,24 +23,33 @@ function makeCorsRequest(word) {
 // All HTML5 Rocks properties support CORS.
   $.each(word, function(index, value){
     var url = 'http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/'+value+'/json';
-    var xhr = createCORSRequest('GET', url);
-    if (!xhr) {
-      console.log('CORS not supported');
-      return;
-    }
-    // Response handlers.
-    xhr.onload = function() {
-      var text = xhr.responseText;
-      var title = getTitle(text);
-      console.log(title);
-    };
-    xhr.onerror = function() {
-      console.log('Woops, there was an error making the request.');
-    };
-    xhr.send();
+    $.ajax({
+
+      type: 'GET',
+      url: 'http://updates.html5rocks.com',
+      contentType: 'text/plain',
+      xhrFields: {
+        // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+        // This can be used to set the 'withCredentials' property.
+        // Set the value to 'true' if you'd like to pass cookies to the server.
+        // If this is enabled, your server must respond with the header
+        // 'Access-Control-Allow-Credentials: true'.
+        withCredentials: false
+      },
+      success: function(data) {
+          var text = data;
+          var title = getTitle(text);
+          console.log(title);
+      },
+
+    });
   });
 
 }
 $(".btn").click(function(){
   makeCorsRequest($(".wrapper").text().split(" "));
 });
+
+
+
+
