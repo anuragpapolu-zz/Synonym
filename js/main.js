1afbca8cd81d6d11e74f15dfc0dda37a8adb0425
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-25 11:39:37 -0800

    asd

--- js/main.js
+++ js/main.js
@@ -25,41 +25,9 @@ String.prototype.replaceAll = function(str1, str2, ignore)
 function isInArray(value, array) {
   return array.indexOf(value);
 }
-function setSelectionRange(input, selectionStart, selectionEnd) {
-  if (input.setSelectionRange) {
-    input.focus();
-    input.setSelectionRange(selectionStart, selectionEnd);
-  }
-  else if (input.createTextRange) {
-    var range = input.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', selectionEnd);
-    range.moveStart('character', selectionStart);
-    range.select();
-  }
-}
-
-function setCaretToPos (input, pos) {
-  setSelectionRange(input, pos, pos);
-}
-(function ($, undefined) {
-    $.fn.getCursorPosition = function() {
-        var el = $(this).get(0);
-        var pos = 0;
-        if('selectionStart' in el) {
-            pos = el.selectionStart;
-        } else if('selection' in document) {
-            el.focus();
-            var Sel = document.selection.createRange();
-            var SelLength = document.selection.createRange().text.length;
-            Sel.moveStart('character', -el.value.length);
-            pos = Sel.text.length - SelLength;
-        }
-        return pos;
-    }
-})(jQuery); 
+// Make the actual CORS request.
 function makeCorsRequest() {
-  var text = $(".wrapper").val();
+  var text = $(".wrapper").text();
   var word = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
   var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
   var i;
@@ -88,19 +56,11 @@ function makeCorsRequest() {
     
   });
   $(".wrapper").html(wordarray.join(" ").replaceAll(' ,', ', ').replaceAll(' " ', '"').replaceAll(' !', '! ').replace(' .', '. ').replace(' ?', '? '));
+
 }
-$('body').keyup(function(e){
-
-   if(e.keyCode == 32){
-       // user has pressed space
-      var position =  $("#edit").getCursorPosition();
-
-      makeCorsRequest();
-      setCaretToPos(document.getElementById("edit"), position);
-
-
-   }
-});
+document.getElementById("edit").addEventListener("input", function() {
+  makeCorsRequest();
+}, false);
 $(document).on('click', '.wrapper a', function(){ 
     $("#results").html('<ul class="list-group"></ul>');
     var value = $(this).attr("href").replace("#","");
