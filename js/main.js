cfe1ef5115b732ca8413600445cc29936d6cdb51
Appajee Papolu <appajee@Appajees-MacBook-Pro-2.local>
2015-01-24 18:21:44 -0800

    ADs

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
-
-// Helper method to parse the title tag from the response.
 function getTitle(text) {
 
   var list = text;
@@ -28,26 +11,18 @@ function makeCorsRequest(word) {
   $.each(word, function (index, value) {
     var url = 'php/get.php?word='+value;
 
-    var xhr = createCORSRequest('POST', url);
-    if (!xhr) {
-      console.log('CORS not supported');
-      return;
-    }
-
-    // Response handlers.
-    xhr.onload = function() {
-      var text = xhr.responseText;
-      var title = getTitle(text);
+    $.ajax({  
+      type: "POST",  
+      url: "some.php",  
+      data: { word: word }
+    }).done(function( msg ) { 
+      var title = getTitle(msg);
       results.push(title);
-      console.log(results);
-    };
-
-    xhr.onerror = function() {
-      console.log('Woops, there was an error making the request.');
-    };
-
-    xhr.send();
+  
+    });
+    
   });
+  console.log(results);
 }
 
 $(".wrapper").bind('input propertychange', function(){
