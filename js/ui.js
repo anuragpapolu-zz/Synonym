dcd3090dfdb0d29ba053079b1c69b3611cad7007
anuragpapolu <anurag.papolu@gmail.com>
2015-01-31 14:34:47 -0800

    asd'

--- js/ui.js
+++ js/ui.js
@@ -1,20 +1,20 @@
-jQuery.fn.wordCount = function(params)
-{
-   var p =  {
-   counterElement:"display_count"
-   };
-  &nbsp;var total_words;
 
-  if(params) {
-      jQuery.extend(p, params);
-  &nbsp;}
-
-  //for each keypress function on text areas
- &nbsp;this.keypress(function()
-  {
-    total_words=this.value.split(/[\s\.\?]+/).length;
-   &nbsp;jQuery('#'+p.counterElement).html(total_words);
-  &nbsp;});
+jQuery.fn.wordCount = function(params){
+	var p = {
+		counterElement:"display_count"
+	};
+	var total_words;
+	
+	if(params) {
+		jQuery.extend(p, params);
+	}
+	
+	//for each keypress function on text areas
+	this.keypress(function()
+	{ 
+		total_words=this.value.split(/[\s\.\?]+/).length;
+		jQuery('#'+p.counterElement).html(total_words);
+	});	
 };
 $(document).ready(function(){
 	$('#edit').wordCount();
