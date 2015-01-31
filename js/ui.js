0166c2a7a61f0848a2594e91a9751a73f8bce9af
anuragpapolu <anurag.papolu@gmail.com>
2015-01-31 15:40:24 -0800

    ADS'

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var area = document.getElementById('edit')
 
 	Countable.live(area, function (counter) {
-	  console.log(counter)
+	  console.log(counter.words)
 	});
 	$('.dropdown')
 	  .dropdown({
