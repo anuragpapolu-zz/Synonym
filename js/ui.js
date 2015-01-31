30bd09abe04e81fe62b61dca0ab8ab31f26e3b85
anuragpapolu <anurag.papolu@gmail.com>
2015-01-31 14:32:41 -0800

    das

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,23 @@
+jQuery.fn.wordCount = function(params)
+{
+   var p =  {
+   counterElement:"display_count"
+   };
+  &nbsp;var total_words;
+
+  if(params) {
+      jQuery.extend(p, params);
+  &nbsp;}
+
+  //for each keypress function on text areas
+ &nbsp;this.keypress(function()
+  {
+    total_words=this.value.split(/[\s\.\?]+/).length;
+   &nbsp;jQuery('#'+p.counterElement).html(total_words);
+  &nbsp;});
+};
 $(document).ready(function(){
+	$('#edit').wordCount();
 	$('.dropdown')
 	  .dropdown({
 	    // you can use any ui transition
@@ -8,4 +27,5 @@ $(document).ready(function(){
 	$('.ui.accordion')
 	  .accordion()
 	;
+
 });
