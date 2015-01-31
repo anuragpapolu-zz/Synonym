652251587743ac5186ad345d553887b56d8df784
anuragpapolu <anurag.papolu@gmail.com>
2015-01-30 23:17:28 -0800

    d

--- js/main.js
+++ js/main.js
@@ -18,88 +18,66 @@ function getTitle(text) {
   var list = eval('(' + text+ ')');
   return list;
 }
-function getCaretPosition(editableDiv) {
-  var caretPos = 0,
-    sel, range;
-  if (window.getSelection) {
-    sel = window.getSelection();
-    if (sel.rangeCount) {
-      range = sel.getRangeAt(0);
-      if (range.commonAncestorContainer.parentNode == editableDiv) {
-        caretPos = range.endOffset;
-      }
-    }
-  } else if (document.selection && document.selection.createRange) {
-    range = document.selection.createRange();
-    if (range.parentElement() == editableDiv) {
-      var tempEl = document.createElement("span");
-      editableDiv.insertBefore(tempEl, editableDiv.firstChild);
-      var tempRange = range.duplicate();
-      tempRange.moveToElementText(tempEl);
-      tempRange.setEndPoint("EndToEnd", range);
-      caretPos = tempRange.text.length;
-    }
-  }
-  return caretPos;
-}
+
 String.prototype.replaceAll = function(str1, str2, ignore) 
 {
-    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+  return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
 } 
 function isInArray(value, array) {
   return array.indexOf(value);
 }
 // Make the actual CORS request.
 function makeCorsRequest() {
-  var text = $("#edit").text();
+  var text = $("#edit").val();
   var word = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
   var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
   var i;
-    for (i = 0; i < word.length; ++i) {
-      if(isInArray(word[i], bad_words) > -1) {
-        word.splice(i,1);
-      } else if (word[i] == "") {
-        word.splice(i,1);
-      }
+  for (i = 0; i < word.length; ++i) {
+    if(isInArray(word[i], bad_words) > -1) {
+      word.splice(i,1);
+    } else if (word[i] == "") {
+      word.splice(i,1);
     }
-    for (i = 0; i < word.length; ++i) {
-      if(isInArray(word[i], bad_words) > -1) {
-        word.splice(i,1);
-      } else if (word[i] == "") {
-        word.splice(i,1);
-      }
+  }
+  for (i = 0; i < word.length; ++i) {
+    if(isInArray(word[i], bad_words) > -1) {
+      word.splice(i,1);
+    } else if (word[i] == "") {
+      word.splice(i,1);
     }
+  }
   var wordarray = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
   $.each(wordarray, function(index, value){
     if(isInArray(value, word) > -1) {
-      wordarray[index] = "<a href='#"+value+"' class='label label-warning'>"+value+"</a>";
+      wordarray[index] = "<a href='#"+value+"' class='ui yellow label'>"+value+"</a>";
       word.splice(isInArray(value, word),1);
 
     } 
 
     
   });
-  $("#edit").html(wordarray.join(" ").replaceAll(' ,', ', ').replaceAll(' " ', '"').replaceAll(' !', '! ').replace(' .', '. ').replace(' ?', '? '));
-
+  $("#wrapper").html(wordarray.join(" ").replaceAll(' ,', ', ').replaceAll(' " ', '"').replaceAll(' !', '! ').replace(' .', '. ').replace(' ?', '? '));
 }
-document.getElementById("edit").addEventListener("input", function() {
+$("#start").click(function(){
+  $("#form").slideUp();
   makeCorsRequest();
-}, false);
-$(document).on('click', '#edit a', function(){ 
-    $("#results").html('<div class="list-group"></div>');
-    var value = $(this).attr("href").replace("#","");
-    var url = 'http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/'+value+'/json';
-    var xhr = createCORSRequest('GET', url);
-    if (!xhr) {
-      console.log('CORS not supported');
-      return;
-    }
+  $("#wrapper").slideDown();
+});
+$(document).on('click', '#wrapper a', function(){ 
+  $("#results").html('<div class="ui vertical fluid menu"></div>');
+  var value = $(this).attr("href").replace("#","");
+  var url = 'http://words.bighugelabs.com/api/2/913ccf11d02b6fc55bef17fcaebe89d9/'+value+'/json';
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    console.log('CORS not supported');
+    return;
+  }
     // Response handlers.
     xhr.onload = function() {
       var text = jQuery.parseJSON(JSON.stringify(eval("(" + xhr.responseText + ")")));
       $.each(text, function(idx, obj) {
         $.each(obj.syn, function(index, wordobject){
-          $("#results div").append('<a href="#'+value+'" class="list-group-item">'+wordobject+'</a>');
+          $("#results div").append('<a href="#'+value+'" class="item">'+wordobject+'</a>');
         });
       });
     };
@@ -107,12 +85,12 @@ $(document).on('click', '#edit a', function(){
       console.log('Woops, there was an error making the request.');
     };
     xhr.send();
- });
+});
 $(document).on('click', '#results a', function(){ 
-    var value = $(this).attr("href").replace("#","");
-    var replacement = $(this).text();
-    $('#edit a[href="#'+value+'"]').text(replacement);
-    $('#edit a[href="#'+value+'"]').removeClass("label-warning").addClass("label-success");
-    $('." a[href="#'+value+'"]').attr('href', '#'+replacement);
-    $("#results").html('<div class="list-group"></div>');
- });
+  var value = $(this).attr("href").replace("#","");
+  var replacement = $(this).text();
+  $('#wrapper a[href="#'+value+'"]').text(replacement);
+  $('#wrapper a[href="#'+value+'"]').removeClass("yellow").addClass("green");
+  $('." a[href="#'+value+'"]').attr('href', '#'+replacement);
+  $("#results").html('<div class="ui vertical fluid menu"></div>');
+});
