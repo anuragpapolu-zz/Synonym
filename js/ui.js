035b79888c93d81fb91bb2f0b78aa332529351bb
anuragpapolu <anurag.papolu@gmail.com>
2015-01-31 15:43:38 -0800

    ADS'

--- js/ui.js
+++ js/ui.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	var area = document.getElementById('edit')
 
 	Countable.live(area, function (counter) {
-	  $("display_words").text(counter.words);
-	  $("display_chars").text(counter.characters);
-	  $("display_paragraphs").text(counter.paragraphs);
+	  $("#display_words").text(counter.words);
+	  $("#display_chars").text(counter.characters);
+	  $("#display_paragraphs").text(counter.paragraphs);
 	});
 	$('.dropdown')
 	  .dropdown({
