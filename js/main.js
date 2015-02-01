e674f68a2ac6ba4a41559f4d426929a10e756c19
anuragpapolu <anurag.papolu@gmail.com>
2015-01-31 19:19:36 -0800

    DSA

--- js/main.js
+++ js/main.js
@@ -14,55 +14,8 @@ xhr = null;
 return xhr;
 }
 // Helper method to parse the title tag from the response.
-function getTitle(text) {
-  var list = eval('(' + text+ ')');
-  return list;
-}
-
-String.prototype.replaceAll = function(str1, str2, ignore) 
-{
-  return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-} 
-function isInArray(value, array) {
-  return array.indexOf(value);
-}
 // Make the actual CORS request.
-function makeCorsRequest() {
-  var text = $("#edit").val();
-  var word = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
-  var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
-  var i;
-  for (i = 0; i < word.length; ++i) {
-    if(isInArray(word[i], bad_words) > -1) {
-      word.splice(i,1);
-    } else if (word[i] == "") {
-      word.splice(i,1);
-    }
-  }
-  for (i = 0; i < word.length; ++i) {
-    if(isInArray(word[i], bad_words) > -1) {
-      word.splice(i,1);
-    } else if (word[i] == "") {
-      word.splice(i,1);
-    }
-  }
-  var wordarray = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
-  $.each(wordarray, function(index, value){
-    if(isInArray(value, word) > -1) {
-      wordarray[index] = "<a href='#"+value+"' class='ui yellow label'>"+value+"</a>";
-      word.splice(isInArray(value, word),1);
 
-    } 
-
-    
-  });
-  $("#wrapper").html(wordarray.join(" ").replaceAll(' ,', ', ').replaceAll(' " ', '"').replaceAll(' !', '! ').replace(' .', '. ').replace(' ?', '? '));
-}
-$("#start").click(function(){
-  $("#form").slideUp();
-  makeCorsRequest();
-  $("#container").slideDown();
-});
 $(document).on('click', '#wrapper a', function(){ 
   $("#results .content .menu .item").slice(1).remove();
   var value = $(this).attr("href").replace("#","");
@@ -89,38 +42,3 @@ $(document).on('click', '#wrapper a', function(){
       .modal('show')
     ;
 });
-$(document).on('click', '#results .content .menu a', function(){ 
-  var value = $(this).attr("href").replace("#","");
-  var replacement = $(this).text();
-
-  $('#wrapper a[href="#'+value+'"]').text(replacement);
-  $('#wrapper a[href="#'+value+'"]').removeClass("yellow").addClass("green");
-  $('#wrapper a[href="#'+value+'"]').attr('href', '#'+replacement);
-  $('.long.modal').modal('hide');
-  $("#results .content .menu .item").slice(1).remove();
-});
-$("#return").click(function(){
-  $("#container").slideUp();
-  $("#edit").val($("#wrapper").text());
-  $("#results .content .menu .item").slice(1).remove();
-  $("#form").slideDown();
-});
-$("#remove").click(function(){
-  $("#wrapper").text("");
-  $("#edit").val("");
-  $("#display_words").text("0");
-  $("#display_chars").text("0");
-  $("#display_paragraphs").text("0");
-});
-
-$('#search').on('input', function() { 
-    var filter = new RegExp($(this).val(), "i");
-
-    $("#results .content .menu a.item").each(function(){
-      if (!$(this).text().match(filter)) {
-        $(this).hide();
-      } else {
-        $(this).show();
-      }
-    });
-  });
---
+++ js/scan.js
@@ -0,0 +1,48 @@
+function getTitle(text) {
+  var list = eval('(' + text+ ')');
+  return list;
+}
+
+String.prototype.replaceAll = function(str1, str2, ignore) 
+{
+  return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+} 
+function isInArray(value, array) {
+  return array.indexOf(value);
+}
+function scan() {
+  var text = $("#edit").val();
+  var word = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
+  var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
+  var i;
+  for (i = 0; i < word.length; ++i) {
+    if(isInArray(word[i], bad_words) > -1) {
+      word.splice(i,1);
+    } else if (word[i] == "") {
+      word.splice(i,1);
+    }
+  }
+  for (i = 0; i < word.length; ++i) {
+    if(isInArray(word[i], bad_words) > -1) {
+      word.splice(i,1);
+    } else if (word[i] == "") {
+      word.splice(i,1);
+    }
+  }
+  var wordarray = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
+  $.each(wordarray, function(index, value){
+    if(isInArray(value, word) > -1) {
+      wordarray[index] = "<a href='#"+value+"' class='ui yellow label'>"+value+"</a>";
+      word.splice(isInArray(value, word),1);
+
+    } 
+
+    
+  });
+  $("#wrapper").html(wordarray.join(" ").replaceAll(' ,', ', ').replaceAll(' " ', '"').replaceAll(' !', '! ').replace(' .', '. ').replace(' ?', '? '));
+}
+$("#start").click(function(){
+  $("#form").slideUp();
+  scan();
+  $("#container").slideDown();
+});
