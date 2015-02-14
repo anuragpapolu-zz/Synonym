c0f8ce3d2d0eb3bb74ef82125e863f6d61ed7863
anuragpapolu <anurag.papolu@gmail.com>
2015-02-14 00:22:01 -0800

    a

--- js/scan.js
+++ js/scan.js
@@ -15,7 +15,7 @@ function scan() {
   var word = text.match(/[\w-']+|[^\w\s]+/g);
   var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
   $.each(word, function(index, value){
-    console.log(word[index])
+    console.log(value)
     if(isInArray(word[index], bad_words) > -1) {
       word.splice(index,1);
     } else if (word[index] == "") {
