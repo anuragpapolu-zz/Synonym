625e02baf94319976c1a2c677370d8b43336a8da
anuragpapolu <anurag.papolu@gmail.com>
2015-02-14 00:03:39 -0800

    ads

--- js/scan.js
+++ js/scan.js
@@ -12,7 +12,7 @@ function isInArray(value, array) {
 }
 function scan() {
   var text = $("#edit").val();
-  var word = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
+  var word = $.trim(text.match(/[\w-']+|[^\w\s]+/g));
   var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
   var i;
   for (i = 0; i < word.length; ++i) {
@@ -22,14 +22,7 @@ function scan() {
       word.splice(i,1);
     }
   }
-  for (i = 0; i < word.length; ++i) {
-    if(isInArray(word[i], bad_words) > -1) {
-      word.splice(i,1);
-    } else if (word[i] == "") {
-      word.splice(i,1);
-    }
-  }
-  var wordarray = $.trim(text.replaceAll(',', ' , ').replaceAll('"', ' " ').replaceAll('!', ' ! ').replace('.', ' . ').replace('?', ' ? ')).split(" ");
+  var wordarray = $.trim(text.match(/[\w-']+|[^\w\s]+/g));
   $.each(wordarray, function(index, value){
     if(isInArray(value, word) > -1) {
       wordarray[index] = "<a href='#"+value+"' class='ui yellow label'>"+value+"</a>";
