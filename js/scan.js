6be560e86606454489860d4d5c96406f8338fb33
anuragpapolu <anurag.papolu@gmail.com>
2015-02-14 14:57:02 -0800

    AS

--- js/scan.js
+++ js/scan.js
@@ -14,20 +14,20 @@ function scan() {
   var text = $("#edit").val();
   var word = text.match(/[\w-']+|[^\w\s]+/g);
   var bad_words = ["all", "another", "own", "be", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
-  $.each(word, function(index, value){
-    if (typeof word[index] !== 'undefined') {
+
+  for(i = 0; i < word.length; i++) {
+    if (typeof word[i] !== 'undefined') {
       alert("ADS");
-      if(isInArray(word[index], bad_words) > -1) {
+      if(isInArray(word[i], bad_words) > -1) {
+        word.splice(i,1);
+      } else if (word[i] == "") {
         word.splice(index,1);
-      } else if (word[index] == "") {
-        word.splice(index,1);
-      } else if (value.length <= 2) {
-        word.splice(index,1);
+      } else if (word[i].length <= 2) {
+        word.splice(i,1);
       }
     }
     
-
-  });
+  }
   for (i = 0; i < word.length/2; i++) { 
     var randomElementIndex = Math.floor( Math.random() * word.length );
     word.splice(randomElementIndex, 1);
