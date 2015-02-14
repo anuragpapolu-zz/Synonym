0e249288cbb4a038a2b1e2599156966840be91df
anuragpapolu <anurag.papolu@gmail.com>
2015-02-14 00:05:14 -0800

    da

--- js/scan.js
+++ js/scan.js
@@ -12,7 +12,7 @@ function isInArray(value, array) {
 }
 function scan() {
   var text = $("#edit").val();
-  var word = $.trim(text.match(/[\w-']+|[^\w\s]+/g));
+  var word = text.match(/[\w-']+|[^\w\s]+/g);
   var bad_words = ["all", "another", "own", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
   var i;
   for (i = 0; i < word.length; ++i) {
@@ -22,7 +22,7 @@ function scan() {
       word.splice(i,1);
     }
   }
-  var wordarray = $.trim(text.match(/[\w-']+|[^\w\s]+/g));
+  var wordarray = text.match(/[\w-']+|[^\w\s]+/g);
   $.each(wordarray, function(index, value){
     if(isInArray(value, word) > -1) {
       wordarray[index] = "<a href='#"+value+"' class='ui yellow label'>"+value+"</a>";
