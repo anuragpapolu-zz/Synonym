bdc823879ab8f7a176c31d361083c04609c288f1
anuragpapolu <anurag.papolu@gmail.com>
2015-02-14 16:52:39 -0800

    ADS

--- js/scan.js
+++ js/scan.js
@@ -15,21 +15,12 @@ function scan() {
   var word = text.match(/[\w-']+|[^\w\s]+/g);
   var bad_words = ["all", "another", "own", "be", "and", "any", "anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];
 
-  for(i = 0; i < word.length; i++) {
-      alert(word[i]);
-      if(isInArray(word[i], bad_words) > -1) {
-        alert("D");
-        word.splice(i,1);
-      } else if (word[i] == "") {
-        word.splice(i,1);
-      } else if (word[i].length) {
-        if(word[i].length <= 2) {
-          word.splice(i,1);
-        }
-      }
+
+  $.each(word, function(index, value){
+    alert(value);
     
-  }
-  for (i = 0; i < word.length/2; i++) { 
+  });
+  for (i = 0; i < Math.floor( Math.random() * word.length/2); i++) { 
     var randomElementIndex = Math.floor( Math.random() * word.length );
     word.splice(randomElementIndex, 1);
     console.log(word);
