c137d90ef9b61bcaf82694eb74847bbbd9c980bb
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-14 09:40:49 -0700

    D

--- js/ui.js
+++ js/ui.js
@@ -2,49 +2,8 @@
 $(document).ready(function(){
 
 
-	$('.dropdown')
-	  .dropdown({
-	    // you can use any ui transition
-	    transition: 'drop'
-	  })
-	;
-	$('.ui.accordion')
-	  .accordion()
-	;
-	$(document).on('click', '#results .content .menu a', function(){ 
-	  var value = $(this).attr("href").replace("#","");
-	  var replacement = $(this).text();
-	  $("#search").val("");
-	  $('#wrapper a[href="#'+value+'"]').text(replacement);
-	  $('#wrapper a[href="#'+value+'"]').removeClass("yellow").addClass("green");
-	  $('#wrapper a[href="#'+value+'"]').attr('href', '#'+replacement);
-	  $('.long.modal').modal('hide');
-	  $("#results .content .menu .item").slice(1).remove();
-	});
-	$("#return").click(function(){
-	  $("#container").slideUp();
-	  $("#edit").val($("#wrapper").text());
-	  $("#results .content .menu .item").slice(1).remove();
-	  $("#form").slideDown();
-	});
-	$("#remove").click(function(){
-	  $("#wrapper").text("");
-	  $("#edit").val("");
-	  $("#display_words").text("0");
-	  $("#display_chars").text("0");
-	  $("#display_paragraphs").text("0");
-	});
-
-	$('#search').on('input', function() { 
-	    var filter = new RegExp($(this).val(), "i");
-
-	    $("#results .content .menu a.item").each(function(){
-	      if (!$(this).text().match(filter)) {
-	        $(this).hide();
-	      } else {
-	        $(this).show();
-	      }
-	    });
-	});
+			new Medium({
+                element: document.getElementById('editor')
+            });
 
 });
