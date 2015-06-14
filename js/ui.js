2e96169c025cd24e938269024dc891ebafe14491
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-14 09:23:37 -0700

    D

--- js/ui.js
+++ js/ui.js
@@ -5,13 +5,7 @@ $(document).ready(function(){
             element.empty();
         }
     });
-	var area = document.getElementById('edit')
 
-	Countable.live(area, function (counter) {
-	  $("#display_words").text(counter.words);
-	  $("#display_chars").text(counter.characters);
-	  $("#display_paragraphs").text(counter.paragraphs);
-	});
 	$('.dropdown')
 	  .dropdown({
 	    // you can use any ui transition
