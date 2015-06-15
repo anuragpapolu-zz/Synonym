06a58bd922e06b48ac04a4af9bf557755f7cf7dc
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-14 17:54:53 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -1,5 +1,18 @@
 $('body').keyup(function(e){
    if(e.keyCode == 32){
-    $("#article").append('</i><i>');
+      $("#article").each(function() {
+          var html = $(this).html().split(" ");
+          var newhtml = [];
+
+          for(var i=0; i< html.length; i++) {
+
+              if(i>0 && (i%2) == 0)
+                  newhtml.push("</i><i>");
+
+              newhtml.push(html[i]);
+          }
+
+          $(this).html("<i>"+ newhtml.join(" ") +"</i>");
+      });
    }
 });
