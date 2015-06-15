48718042fdba61a0b71ab10474c1da2b3a6aa397
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-14 17:39:58 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -1,14 +1,5 @@
-(function ($) {
-    $.fn.type = function () {
-        return this.each(function () {
-            var $this = $(this);
-            var htmlold = $this.html();
-            $this.bind('blur keyup paste copy cut mouseup', function () {
-                var htmlnew = $this.html();
-                if (htmlold !== htmlnew) {
-                    $this.trigger('change')
-                }
-            })
-        })
-    }
-})(jQuery);
+$('body').keyup(function(e){
+   if(e.keyCode == 32){
+    $("#article").append('</i><i>');
+   }
+});
