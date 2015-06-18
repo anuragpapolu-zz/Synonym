8de687de85097de9c8d5d6930cfb3b75b8216348
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-17 23:48:37 -0700

    D

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $('body').on('click', 'a.synonyms', function() {
-    $("#checker").find("#"+$(this).attr("id")).text($(this).text);
+    $("#checker").find("#"+$(this).parent().attr("id")).text($(this).text);
     $("#trash").html($("#checker").html());
     $("#trash span").each(function() {
     	$(this).contents().unwrap();
     });
-    $("#article").html($("#trash").html());
+    $("article").html($("#trash").html());
 });
