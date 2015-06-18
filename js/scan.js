1cfddc9100b335a4e67d9c3e419e75fbe4a676a0
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-17 19:46:38 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -29,7 +29,11 @@ function doneTyping () {
               url: "http://api.wordnik.com:80/v4/word.json/"+$(this).text()+"/relatedWords?useCanonical=true&relationshipTypes=synonym&api_key=7026726c936e0ea32700d53c3c60294e50e5db2f2dab65fc5",
               type: "get",
               success: function(data) {
-                console.log(data[0][Object.keys(data[0])[0]]);
+                if(data[0][Object.keys(data[0])[1]].length > 0) {
+                  $(this).addClass('checked');
+                } else {
+                  $(this).addClass('none');
+                }
               }
             });
             
