09b6e6a3b26e2c3d60be88660569be38164c135c
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-18 01:42:22 -0700

    D

--- js/ui.js
+++ js/ui.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 	new Medium({
 	    element: document.getElementById('article'),
 	    mode: Medium.richMode,
-	    placeholder: 'Your Article',
 		tags: {
 			'horizontalRule': ""
 
