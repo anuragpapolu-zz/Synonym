a1e8eadb3f184be484104e15278d2e490ca836d5
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-17 17:57:55 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -8,5 +8,14 @@ document.getElementById("article").addEventListener("input", function() {
 function doneTyping () {
   var content = $("#mimic").html();
   $("#mimic").html($("#article").html());
+  $('#mimic').each(function(){
+    var text = $(this).html().split(' '),
+        len = text.length,
+        result = []; 
+    for( var i = 0; i < len; i++ ) {
+      result[i] = '<span>' + text[i] + '</span>';
+    }
+    $(this).html(result.join(' '));
+  });
   $("#checker").html(content);
 }
