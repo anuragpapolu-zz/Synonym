2a7f9b52b113874805b3114c7f784021a02aceff
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-17 18:29:59 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -8,8 +8,8 @@ document.getElementById("article").addEventListener("input", function() {
 var bad_words = ["at", "an","all", "another", "own", "be", "and", "any","from", "there","anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "are","few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","with","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];  
 
 function doneTyping () {
-  var content = $("#mimic").html();
   $("#mimic").html($("#article").html());
+  var content = $("#mimic").html();
   $('#mimic').each(function(){
     var text = $(this).html().split(' '),
         len = text.length,
