a95f1c311b88b0217ae796e8c465d89375f7f9c4
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-18 01:05:17 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -22,7 +22,7 @@ function syno () {
     var words = $("#article").html().split(" ");
     var newcontent = [];
     $.each(words, function(i, v) {
-        newcontent.push($('<span id="'+i+'">'+v+'</span>')));
+        newcontent.push($('<span id="'+i+'">'+v+'</span>'));
     });
     $("#mimic").html(newcontent.join(" "));
     $("#checker").html(content);
