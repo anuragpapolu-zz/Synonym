b5088b44e06aa319c7b817b2e47b679b9dec101e
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-17 23:55:36 -0700

    D

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('body').on('click', 'a.synonyms', function() {
 	console.log("D");
-    $("#checker").find("#"+$(this).parent().attr("id")).text($(this).text);
+    $("#checker").find("#"+$(this).parent().attr("id")).text($(this).text());
     console.log("S");
     $("#trash").html($("#checker").html());
     $("#trash span").each(function() {
