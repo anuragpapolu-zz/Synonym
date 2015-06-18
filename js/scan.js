4c661fbad07b0e62e493a9247351fbced143275b
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-18 14:39:04 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -2,7 +2,7 @@ var typingTimer;                //timer identifier
 var doneTypingInterval = 500;
 
 document.getElementById("article").addEventListener("input", function() {
-    $("#checker").css("margin-top", "-"+$("#article").height()+"px");
+    $("#checker").css("margin-top", "-"+$("#article").height() + 10 +"px");
     clearTimeout(typingTimer);
      typingTimer = setTimeout(doneTyping, doneTypingInterval);
      
