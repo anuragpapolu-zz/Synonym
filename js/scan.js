d362bb3e50a922dacf83d736b25dc9deefbe80df
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-17 18:00:58 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -1,4 +1,4 @@
-var typingTimer;                //timer identifier
+/* var typingTimer;                //timer identifier
 var doneTypingInterval = 500;
 document.getElementById("article").addEventListener("input", function() {
     clearTimeout(typingTimer);
@@ -19,3 +19,4 @@ function doneTyping () {
   });
   $("#checker").html(content);
 }
+*/
--- js/ui.js
+++ js/ui.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
 	new Medium({
 	    element: document.getElementById('article'),
 	    mode: Medium.richMode,
-	    placeholder: 'Your Essay',
+	    placeholder: 'Your Article',
 		tags: {
 			'horizontalRule': ""
 
 		}
 	});
-
 });
