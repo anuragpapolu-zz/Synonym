375d68a26a6b943461d599dac306ae6bea607d7d
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-06-18 00:16:11 -0700

    D

--- js/scan.js
+++ js/scan.js
@@ -7,18 +7,7 @@ document.getElementById("article").addEventListener("input", function() {
      typingTimer = setTimeout(doneTyping, doneTypingInterval);
      
 }, false);
-$('body').on('click', 'a.synonyms', function() {
-    $("#checker").find("#"+$(this).parent().attr("id")).text($(this).text());
-    $("#trash").html($("#checker").html());
-    $("#trash span").each(function() {
-      $(this).contents().unwrap();
-    });
-    $("#article").html($("#trash").html());
-    console.log("a");
-    doneTyping();
-    console.log("b");
-     
-});
+
 var bad_words = ["at", "an","all", "another", "own", "be", "and", "any","from", "there","anybody", "anyone", "anything", "a", "both", "but", "each", "can't", "either", "everybody", "everyone", "everything", "are","few", "for", "he", "her", "herself", "hers", "him", "himself", "his","how", "I", "it", "itself", "its", "it’s", "many", "me", "mine", "more", "most", "much", "myself", "neither", "no one", "nobody", "none", "nothing","nor", "one", "one another","or", "other", "others", "ours", "ourselves", "several", "she", "so", "some", "somebody", "someone", "something", "that", "their",  "theirs", "them", "themselves", "these", "this", "they", "those", "to", "us", "we", "what", "whatever", "when", "which", "whichever", "who", "whoever", "whom", "whomever", "whose", "you", "your", "yours", "yourself", "yourselves", "yet", "back", "in", "the", "of", "our", "ours", "just", "on", "as","one","two","three","four","five","six","seven","eight","with","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion","trillion","million",",",'"',"!","?"];  
 
 function doneTyping () {
@@ -73,3 +62,62 @@ function doneTyping () {
        typingTimer = setTimeout(doneTyping, doneTypingInterval);  
   }
 }
+$('body').on('click', 'a.synonyms', function() {
+    $("#checker").find("#"+$(this).parent().attr("id")).text($(this).text());
+    $("#trash").html($("#checker").html());
+    $("#trash span").each(function() {
+      $(this).contents().unwrap();
+    });
+    $("#article").html($("#trash").html());
+    if($("#checker").text()!=$("#article").text()) {
+      var content = $("#mimic").html();
+      $("#mimic").html($("#article").html());
+      $('#mimic').each(function(){
+        var text = $(this).html().split(' '),
+            len = text.length,
+            result = []; 
+        for( var i = 0; i < len; i++ ) {
+          result[i] = '<span id="'+i+'">' + text[i] + '</span>';
+        }
+        $(this).html(result.join(' '));
+      });
+      $("#checker").html(content);
+      $('.ui.accordion').html("");
+      $("#checker span").each(function(){
+        var id = $(this).attr('id');
+        var text = $(this).text();
+        if($("#mimic #"+$(this).attr('id')).text() == $(this).text()) {
+          if($(this).attr('class')!="checked" && $(this).attr('class')!="none") {
+            if(jQuery.inArray($(this).text(), bad_words) == -1 && isNaN($(this).text().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,""))) {
+              $.ajax({
+                url: "http://api.wordnik.com:80/v4/word.json/"+$(this).text()+"/relatedWords?useCanonical=true&relationshipTypes=synonym&api_key=7026726c936e0ea32700d53c3c60294e50e5db2f2dab65fc5",
+                type: "get",
+                success: function(data) {
+                  if(data.length != 0) {
+                    $("#checker #"+id).addClass('checked');
+                    $(".accordion").append('<div class="title">'+text+'</div><div id="'+id+'"class="content ui list"></div>');
+                    for (var i = data[0].words.length - 1; i >= 0; i--) {
+                      $("#results #"+id).append("<a class='item synonyms'>"+data[0].words[i]+"</a>");
+                    };
+                    
+                    $('.ui.accordion')
+                      .accordion()
+                    ;
+                    $(".ui.accordion .title:first").addClass('active');
+                    $(".ui.accordion .content:first").addClass('active');
+
+                  }
+                }
+              });
+              
+            } else {
+              $(this).addClass('none');
+            }
+          }
+        }
+      });
+        clearTimeout(typingTimer);
+         typingTimer = setTimeout(doneTyping, doneTypingInterval);  
+    }
+     
+});
